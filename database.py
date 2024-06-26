7d89dd120a3861f28acbab7015d05ffd3c7eb807
lvzvrv <semyon.lazarev.03@mail.ru>
2024-06-26 19:42:51 +0700

    Update database.py

--- database.py
+++ database.py
@@ -5,14 +5,11 @@ import datetime
 
 DATABASE_URL = "postgresql://postgres:BratkaIlya2015@localhost:5432/mydatabase"
 
-# Создание двигателя SQLAlchemy
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 
-# Создание сессии
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Определение модели User
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
@@ -21,5 +18,4 @@ class User(Base):
     last_name = Column(String)
     registration_date = Column(DateTime, default=datetime.datetime.utcnow)
 
-# Создание таблиц в базе данных
 Base.metadata.create_all(bind=engine)
